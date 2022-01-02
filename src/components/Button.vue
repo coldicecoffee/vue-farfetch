81f43df80ddf50e6a4414ae45e7ab7354bf0185e
Jude Hokyoon Woo <kw021992@gmail.com>
2022-01-02 21:08:21 +0900

    Add Button component's foundation

---
+++ src/components/Button.vue
@@ -0,0 +1,28 @@
+<template>
+  <a href="#" @click="onClick">
+    {{ text }}
+  </a>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+  props: {
+    text: String,
+    onClick: Function,
+  },
+})
+export default class Button extends Vue {
+  text!: string;
+  onClick!: Function;
+}
+</script>
+
+<style scoped lang="scss">
+a {
+  &:hover {
+    text-decoration: unset;
+  }
+}
+</style>
