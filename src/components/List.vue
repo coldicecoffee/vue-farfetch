bfa7f25c92e3840556b23e9608a3d6163c5b8f66
Jude Hokyoon Woo <kw021992@gmail.com>
2022-01-05 18:12:57 +0900

    Add List Component

---
+++ src/components/List.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul>
+    <li v-for="item in items" :key="item.id">
+      {{ item.message }}
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+  props: {
+    items: Array,
+  },
+})
+export default class Button extends Vue {
+  text!: Array<Object>;
+}
+</script>
+
+<style scoped lang="scss">
+ul {
+  list-style-type: none;
+}
+</style>
