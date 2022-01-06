5e81af1f5c4db21779238d61c2efda90d804b6e8
Jude Hokyoon Woo <kw021992@gmail.com>
2022-01-06 20:28:37 +0900

    Add Text Component

---
+++ src/components/Text.vue
@@ -0,0 +1,23 @@
+<template>
+  <p>{{ text }}</p>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+  props: {
+    text: String,
+  },
+})
+export default class Text extends Vue {
+  text!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+p {
+  margin: 0;
+}
+</style>
